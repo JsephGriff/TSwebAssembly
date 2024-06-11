698980bd885ee15fe0a19af59ebd95baf1905a8e
JsephGriff <JsephGriff@outlook.com>
2024-06-11 11:46:12 -0700

    Organize Project Layout
    Rearrange the layout of testing and organized my file linking.

---
+++ tests/tokenizer_test.ts
@@ -0,0 +1,20 @@
+import * as assert from 'assert';
+import { tokenize } from '../src/tokenizer';
+
+describe('tokenize', function() {
+
+    it('handlesToken', function() {
+        const input = " print";
+        const tokens = tokenize(input);
+        assert.strictEqual(tokens.length, 1);
+    });
+
+    it('handlesMultipleTokens', function() {
+        const input = " print 3.14 ";
+        const tokens = tokenize(input);
+        assert.strictEqual(tokens.length, 2);
+        assert.strictEqual(tokens[0].type, "keyword");
+        assert.strictEqual(tokens[1].type, "number");
+    });
+
+});
